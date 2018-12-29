52ae1a62613f39c136ec8774b247ad50aab9a74d
ZhaoXianYun <xy@inshowlife.com>
2018-12-29 18:42:54 +0800

    add react-navigation

---
+++ project/page/views/ImageButton.js
@@ -0,0 +1,51 @@
+/**
+ * Created by inshow on 2018/12/29.
+ */
+
+import React from 'react';
+import {
+    Image,
+    TouchableWithoutFeedback,
+} from 'react-native';
+export default class ImageButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            buttonPressed: false,
+        }
+    }
+    static initialState = {
+        buttonPressed: false,
+    };
+    static defaultProps = {
+        source: null,
+        highlightedSource: null,
+        onPress: null,
+    };
+    _buttonPressIn() {
+        this.setState({ buttonPressed: true });
+    }
+    _buttonPressOut() {
+        this.setState({ buttonPressed: false });
+    }
+    _isButtonPressed() {
+        return this.state.buttonPressed;
+    }
+    render() {
+        let source = this.props.source;
+        if (this._isButtonPressed() && this.props.highlightedSource) {
+            source = this.props.highlightedSource;
+        }
+        return (
+            <TouchableWithoutFeedback
+                onPress={this.props.onPress}
+                onPressIn={this._buttonPressIn.bind(this)}
+                onPressOut={this._buttonPressOut.bind(this)}
+            >
+                <Image
+                    style={this.props.style}
+                    source={source} />
+            </TouchableWithoutFeedback>
+        );
+    }
+};
